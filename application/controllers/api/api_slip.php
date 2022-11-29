d4e143418fbc1404a7e0cfe7ec66e74c9722c769
mafulum <mafulum.muttaqin@gmail.com>
2022-11-29 09:42:20 +0700

    slip year empty

--- application/controllers/api/api_slip.php
+++ application/controllers/api/api_slip.php
@@ -20,7 +20,7 @@ class api_slip extends CI_Controller {
     public function list_year($nopeg){
         $dir    = getcwd().'/payslip/'.$nopeg;
         if(is_dir($dir)==false){
-            echo json_encode([[date('Y')]]);
+            echo json_encode([[date('Y')]);
         }else{
             $files = scandir($dir);
             $aRes = [];
@@ -29,7 +29,7 @@ class api_slip extends CI_Controller {
                     $aRes[]=$name;
                 }
             }
-            echo json_encode([$aRes]);
+            echo json_encode($aRes);
         }        
     }
     
