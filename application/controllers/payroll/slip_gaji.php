27a67e98e583e36fb50de26e722ed49e36a98075
mafulum <mafulum.muttaqin@gmail.com>
2023-02-27 10:26:17 +0700

    slip gaji page

--- application/controllers/payroll/slip_gaji.php
+++ application/controllers/payroll/slip_gaji.php
@@ -26,7 +26,6 @@ class slip_gaji extends CI_Controller {
     }
 
     public function index(){
-        
         $data['base_url'] = $this->config->item('base_url');
         $data["userid"] = $this->session->userdata('username');
         $data['view'] = 'payroll/slip_gaji';
@@ -73,7 +72,7 @@ class slip_gaji extends CI_Controller {
                             vis_reg = "off";
                         }
                         $.ajax({
-                            url: "'. base_url().'index.php/payroll/tax_yearly/go",
+                            url: "'. base_url().'index.php/payroll/slip_gaji/go",
                             type: "get", //send it through get method
                             timeout : 0,
                             data: { 
@@ -100,6 +99,35 @@ class slip_gaji extends CI_Controller {
 		});
 		</script>';
         $this->load->view('main', $data);
+    }
+
+    
+    public function go() {
+        set_time_limit(0);
+        ini_set("max_execution_time",3600);
+        $period = $this->input->get('year');
+        $nopeg = $this->input->get('fnik');
+        $year = substr($period,0,4);
+        echo $year."|".$period."|".$nopeg;
+        // $data = http_build_query($paramAPI);
+        // $curl = curl_init();
+        // $url = $this->config->item('base_url_engine_payroll').'/payroll/tax_yearly'."?".$data;
+        // curl_setopt_array($curl, array(
+        //   CURLOPT_URL => $url,
+        //   CURLOPT_RETURNTRANSFER => true,
+        //   CURLOPT_ENCODING => '',
+        //   CURLOPT_MAXREDIRS => 10,
+        //   CURLOPT_TIMEOUT => 0,
+        //   CURLOPT_FOLLOWLOCATION => true,
+        //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //   CURLOPT_CUSTOMREQUEST => 'GET',
+        // ));
+
+        // $response = curl_exec($curl);
+        // curl_close($curl);
+        
+        // header('Content-Type: application/json');
+        // echo $response;
     }
     
     public function sendMailSlip($id_document_transfer){
--- application/views/payroll/slip_gaji.php
+++ application/views/payroll/slip_gaji.php
@@ -18,11 +18,10 @@
                                       <button class="btn btn-danger" type="button"><i class="fa fa-calendar"></i></button>
                                     </span>
                             </div>
-                            <span class="help-block">Periode</span>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="fnik" class="col-lg-2 col-sm-2 control-label">Emp</label>
+                        <label for="fnik" class="col-lg-2 col-sm-2 control-label">Nopeg (comma separated)</label>
                         <div class="col-md-3 col-xs-11">
                             <input type="text" class="form-control" id="fnik" name="fnik" value="" style="padding: 0.5px 0px;">
                         </div>
