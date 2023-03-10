872792e968a10d5c08555634db4a63ab39cb8d0e
mafulum <mafulum.muttaqin@gmail.com>
2023-03-10 16:12:36 +0700

    spt send email

--- application/controllers/crontab/send_mail_spt_2022.php
+++ application/controllers/crontab/send_mail_spt_2022.php
@@ -32,22 +32,23 @@ class send_mail_spt_2022 extends CI_Controller {
         foreach($this->maps as $map){
             var_dump($map);
             echo "<br/>";
-            // $curl = curl_init();
-            // curl_setopt_array($curl, array(
-            //   CURLOPT_URL => 'http://172.17.0.1:8181/api/machine/send_spt_2022',
-            //   CURLOPT_RETURNTRANSFER => true,
-            //   CURLOPT_ENCODING => '',
-            //   CURLOPT_MAXREDIRS => 10,
-            //   CURLOPT_TIMEOUT => 0,
-            //   CURLOPT_FOLLOWLOCATION => true,
-            //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            //   CURLOPT_CUSTOMREQUEST => 'POST',
-            //   CURLOPT_POSTFIELDS => array('pernr' => $map['pernr'],'email' => $map['email'],'title' => 'SPT 2022 - PT. Garuda Daya Pratama Sejahtera',
-            //   'emp_nama' => $map['emp_nama'],'file' => 'SPT_1','year' => '2022'),
-            // ));
-            // $response = curl_exec($curl);
-            // curl_close($curl);
-            // echo $response;
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'http://172.17.0.1:8181/api/machine/send_spt_2022',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'POST',
+              CURLOPT_POSTFIELDS => array('pernr' => $map['pernr'],'email' => $map['email'],'title' => 'SPT 2022 - PT. Garuda Daya Pratama Sejahtera',
+              'emp_nama' => $map['emp_nama'],'file' => 'SPT_1','year' => '2022'),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+            echo "<br/>";
         }
     }
 
